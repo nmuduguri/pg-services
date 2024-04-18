7bb4e0af0944e1cf588be8e8f1efa845a30e2fb8
venkatamudhigiri <venkatamudhigiri@gmail.com>
2024-04-18 12:17:27 +0530

    new project added

---
+++ pg-mgmt/src/main/java/com/ngalaxy/PgMgmtApplication.java
@@ -0,0 +1,15 @@
+package com.ngalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgMgmtApplication.class, args);
+		
+		System.out.println("We are in PG Management Application..");
+	}
+
+}
---
+++ pg-mgmt/src/test/java/com/ngalaxy/PgMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngalaxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
